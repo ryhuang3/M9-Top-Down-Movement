c290eca859bd8c53030aa8379c78a967c94d3201
Ryan Huang <ryhuang3@s.sfusd.edu>
2025-12-18 11:59:25 -0800

    M9L12

--- runner.gd
+++ runner.gd
@@ -1,27 +1,22 @@
-class_name Runner
 extends CharacterBody2D
 
 @export var max_speed := 600.0
 @export var acceleration := 1200.0
-@export var deceleration := 1080.0
 
-@onready var _runner_visual: RunnerVisual = %RunnerVisualRed
 @onready var _dust: GPUParticles2D = %Dust
-
-signal walked_to
+@onready var _runner_visual: RunnerVisual = %RunnerVisualPurple
 
 func _physics_process(delta: float) -> void:
-	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	var has_input_direction := direction.length() > 0.0
-	if has_input_direction:
-		var desired_velocity := direction * max_speed
-		velocity = velocity.move_toward(desired_velocity, acceleration * delta)
-	else:
-		velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
+	var direction := global_position.direction_to(get_global_mouse_position())
+	var distance := global_position.distance_to(get_global_mouse_position())
+	var speed := max_speed if distance > 100 else max_speed * distance / 100
 
+	var desired_velocity := direction * speed
+
+	velocity = velocity.move_toward(desired_velocity, acceleration * delta)
 	move_and_slide()
 
-	if direction.length() > 0.0:
+	if velocity.length() > 10.0:
 		_runner_visual.angle = rotate_toward(_runner_visual.angle, direction.orthogonal().angle(), 8.0 * delta)
 
 		var current_speed_percent := velocity.length() / max_speed
@@ -30,26 +25,8 @@ func _physics_process(delta: float) -> void:
 			if current_speed_percent < 0.8
 			else RunnerVisual.Animations.RUN
 		)
+
 		_dust.emitting = true
 	else:
 		_runner_visual.animation_name = RunnerVisual.Animations.IDLE
 		_dust.emitting = false
-
-func walk_to(destination_global_position: Vector2) -> void:
-	var direction := global_position.direction_to(destination_global_position)
-	_runner_visual.angle = direction.orthogonal().angle()
-
-	_runner_visual.animation_name = RunnerVisual.Animations.WALK
-	_dust.emitting = true
-
-	var distance := global_position.distance_to(destination_global_position)
-	var duration :=  distance / (max_speed * 0.2)
-
-	var tween := create_tween()
-	tween.tween_property(self, "global_position", destination_global_position, duration)
-	tween.finished.connect(func():
-		_runner_visual.animation_name = RunnerVisual.Animations.IDLE
-		_dust.emitting = false
-		walked_to.emit()
-	)
-	
